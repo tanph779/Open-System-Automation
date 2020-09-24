2228345ce3db4ff11981f5c3956a65ab7391bee6
tanph779 <tanph779@mail.com>
2020-09-24 15:33:29 -0400

    first commit

---
+++ Lab_1/lab1b.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+name = 'Isaac'
+
+print('How old are you ' + name + '?')
---
+++ Lab_1/lab1d.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+x = 10 
+y = 2 
+z = 5
+sum = x + y * z
+print(str(x) + ' + ' + str(y) + ' * ' + str(z) + ' = ' + str(sum))
